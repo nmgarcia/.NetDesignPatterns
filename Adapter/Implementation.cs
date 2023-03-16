df3bac4cedb2b6c155e8ef6beeb7044a39e9e8da
nmgarcia <nemarianogarcia@gmail.com>
2023-03-16 19:26:26 -0300

    Adapter Pattern Implmented

--- Adapter/Implementation.cs
+++ Adapter/Implementation.cs
@@ -1,3 +1,52 @@
-﻿namespace Adapter
+﻿/*
+ n the example we looked at, we had two interfaces for calculating the area of a shape - IShape and IAdvancedShape. 
+We had an existing class Rectangle that implemented the IShape interface, but we wanted to use it as an IAdvancedShape interface.
+So, we created an adapter class RectangleAdapter that implemented the IAdvancedShape interface and internally used the Rectangle class to calculate the area.
+
+This allowed us to use the Rectangle class as if it were an IAdvancedShape object, without modifying its original implementation.
+The adapter class acted as a bridge between the two interfaces, and the client was able to use the adapted object seamlessly.
+ */
+namespace Adapter
 {
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+
+    public class Rectangle : IShape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public double CalculateArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    public class Triangle
+    {
+        public double Base { get; set; }
+        public double Height { get; set; }
+
+        public double GetArea()
+        {
+            return 0.5 * Base * Height;
+        }
+    }
+
+    public class TriangleAdapter : IShape
+    {
+        private readonly Triangle _triangle;
+
+        public TriangleAdapter(Triangle triangle)
+        {
+            _triangle = triangle;
+        }
+
+        public double CalculateArea()
+        {
+            return _triangle.GetArea();
+        }
+    }
 }
--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace ClassAdapter
+﻿namespace Adapter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var rectangle = new Rectangle { Width = 5, Height = 10 };
+            var triangle = new Triangle { Base = 8, Height = 6 };
+            var triangleAdapter = new TriangleAdapter(triangle);
+
+            Console.WriteLine($"Rectangle area: {rectangle.CalculateArea()}");
+            Console.WriteLine($"Triangle area: {triangleAdapter.CalculateArea()}");
         }
     }
 }
